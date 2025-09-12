3a1db142c2aec8c9bb91a451395557c111815840
Artur <20243ds154utez.edu.mx>
2025-09-11 20:14:54 -0600

    se termino promedio sueldos

--- tproSueldos/src/App.java
+++ tproSueldos/src/App.java
@@ -1,45 +1,51 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
 
-        // Inicializa
-        List<Worker> Worker = new ArrayList<>();
+        List<Worker> workers = new ArrayList<>();
 
 
-        // Añade workers
-        worker.add(new worker(1, Naul, 1000));
-        worker.add(new worker(2, Calos, 2000));
-        worker.add(new worker(3, Emiliano, 3000));
-        worker.add(new worker(4, Deguito, 4000));
-        worker.add(new worker(5, Kevin, 5000));
-        worker.add(new worker(6, Gibran, 6000));
-        
-
-        double total = 0; // Inicializamos la variable sueldo
+        // Inicializa trabajadores
+        workers.add(new Worker(1, "Naul", 1000));
+        workers.add(new Worker(2, "Carlos", 800));
+        workers.add(new Worker(3, "Emiliano", 3000));
+        workers.add(new Worker(4, "Deguito", 560));
+        workers.add(new Worker(5, "Kevin", 50));
+        workers.add(new Worker(6, "Gibran", 6000));
 
 
-        // Recorrer workers
-        for(String work : workers){ //for edge
-            System.out.println();
+        // Calcula el promedio
+        double total = 0;
+        for (Worker w : workers) {
+            total += w.getSueldo();
         }
-        
+        double promedio = total / workers.size();
+        System.out.println("Promedio de sueldos: " + promedio);
 
 
+        //  Elimina sueldos menores a 1000
+        for (int i = 0; i < workers.size(); i++) {
+            if (workers.get(i).getSueldo() < 1000) {
+                workers.remove(i);
+                i--; // retroceder indice ya que la lista se va haciendo mas chica
+            }
+        }
 
 
-
-        System.out.println("Stack: " + total);
-
-
-
-
-
-
+        // Muestra la  lista actualizada
+        System.out.println("\nLista actualizada:");
+        for (Worker w : workers) {
+            System.out.println(w);
+        }
 
 
     }
+
+
 }
