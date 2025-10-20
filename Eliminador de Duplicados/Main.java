85f60376604f684bc0e92463c5b469ac7a641411
KevinR2D2PC <20243ds154@utez.edu.mx>
2025-10-20 09:03:08 -0600

    Add files via upload
    ya quedo

---
+++ Eliminador de Duplicados/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.add(5);
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(3);
+        list.add(9);
+
+        System.out.println("Lista original:");
+        list.printList();
+
+        list.removeDuplicates();
+
+        System.out.println("Lista sin duplicados:");
+        list.printList();
+    }
+}
---
+++ Eliminador de Duplicados/SinglyLinkedList.java
@@ -0,0 +1,41 @@
+public class SinglyLinkedList {
+    Node head;
+
+    public void add(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void removeDuplicates() {
+        Node current = head;
+
+        while (current != null) {
+            Node runner = current;
+            while (runner.next != null) {
+                if (runner.next.data == current.data) {
+                    runner.next = runner.next.next; 
+                } else {
+                    runner = runner.next;
+                }
+            }
+            current = current.next;
+        }
+    }
+
+    public void printList(){
+        Node current = head;
+        while(current != null){
+            System.out.print(current.data);
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
