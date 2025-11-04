a1b6ed68af5bf0d9096905afce084851444df3c7
KevinR2D2PC <20243ds154@utez.edu.mx>
2025-11-03 21:54:22 -0600

    Update App.java
    redy

--- Ejercicio de practica 1 (Lista enlazada)/Ejercicio de practica 1 (Lista enlazada)/App.java
+++ Ejercicio de practica 1 (Lista enlazada)/Ejercicio de practica 1 (Lista enlazada)/App.java
@@ -2,12 +2,16 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         SinglyLinkedList lista = new SinglyLinkedList();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
         lista.add(10);
-        lista.add(15);
-        lista.add(20);
-        lista.add(25);
-        lista.add(30);
-        lista.add(35);
 
         System.out.println("Lista Original:");
         lista.printList();
@@ -33,3 +37,4 @@ public class App {
         listaImpares.printList();
     }
 }
+
